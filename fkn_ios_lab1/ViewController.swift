4373b3385335a597816ff7c3e9406739213d99af
SergeBankovskiy <it.impostor@gmail.com>
2023-01-03 02:27:18 +0600

    Initial Commit

---
+++ fkn_ios_lab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fkn_ios_lab1
+//
+//  Created by Серж Банковский on 03.01.2023.
+//  Copyright © 2023 Серж Банковский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
